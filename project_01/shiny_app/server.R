52231a955ed980878fc1bfe759c974e49c98f577
JK <a9ce4560@opayq.com>
2017-04-04 11:20:45 -0400

    data read updates

--- project_01/shiny_app/server.R
+++ project_01/shiny_app/server.R
@@ -1,10 +1,48 @@
 # server
 
 library(maps)
+library(dplyr)
 library(mapproj)
 
-counties = readRDS("/Users/intothelight/nycdatascience/R_work/shiny_apps/counties_app/data/counties.rds")
-source("/Users/intothelight/nycdatascience/R_work/shiny_apps/counties_app/helpers.R")
+
+# common 
+columns_to_use = c(rep(NA, 2), rep("NULL", 2),rep(NA, 58))
+
+# female data
+female_lexp = read.csv('../data/API_SP.DYN.LE00.FE.IN_DS2_en_csv_v2.csv', 
+                       header = TRUE, skip = 4, stringsAsFactors = FALSE, colClasses = columns_to_use)
+female_lexp[58:60] <- NULL # get rid of empty last 3 columns
+female_lexp = female_lexp[order(female_lexp$Country.Name),]
+rownames(female_lexp) = female_lexp$Country.Code
+country_names = female_lexp[,1:2]
+rownames(country_names) = country_names$Country.Code
+female_lexp[1:2] <- NULL
+country_names[2] <- NULL
+empty_DF <- female_lexp[rowSums(is.na(female_lexp)) == ncol(female_lexp),]
+country_names = country_names[!(rownames(country_names) %in% rownames(empty_DF)),,drop=FALSE]
+female_lexp = female_lexp[rowSums(is.na(female_lexp)) != ncol(female_lexp),]
+
+
+# male data
+male_lexp = read.csv('../data/API_SP.DYN.LE00.MA.IN_DS2_en_csv_v2.csv', 
+                     header = TRUE, skip = 4, stringsAsFactors = FALSE, colClasses = columns_to_use)
+male_lexp[58:60] <- NULL # get rid of empty last 3 columns
+male_lexp = male_lexp[order(male_lexp$Country.Name),]
+rownames(male_lexp) = male_lexp$Country.Code
+male_lexp[1:2] <- NULL
+male_lexp = male_lexp[rowSums(is.na(male_lexp)) != ncol(male_lexp),]
+
+# don't include indicator name/symbol, and drop 2015,2016 which are just empty
+#columns_to_use = c(rep("NA", 2), rep("NULL", 2),rep(NA, 57), rep("NULL", 3))
+#  [0,1] + range(4, 59) 
+
+# read and skip non-essential data
+#female_lexp = read.csv('../data/API_SP.DYN.LE00.FE.IN_DS2_en_csv_v2.csv', header = TRUE, skip = 4,stringsAsFactors = FALSE) 
+#pd.read_csv('data/API_SP.DYN.LE00.FE.IN_DS2_en_csv_v2.csv', skiprows=2, header=1, usecols=columns_to_use)
+#male_lexp = pd.read_csv('data/API_SP.DYN.LE00.MA.IN_DS2_en_csv_v2.csv', skiprows=2, header=1, usecols=columns_to_use)
+
+#counties = readRDS("/Users/intothelight/nycdatascience/R_work/shiny_apps/counties_app/data/counties.rds")
+#source("/Users/intothelight/nycdatascience/R_work/shiny_apps/counties_app/helpers.R")
 
 # centre = function(x,type){
 #   switch(type,
@@ -16,19 +54,21 @@ source("/Users/intothelight/nycdatascience/R_work/shiny_apps/counties_app/helper
 # centre(x, "median")
 
 shinyServer(function(input, output) {
-  output$map = renderPlot( {
-
-args = switch(input$var,
-              "Percent White" = list(counties$white, "darkgreen", "% White"),
-              "Percent Black" = list(counties$black, "black", "% Black"),
-              "Percent Hispanic" = list(counties$hispanic, "darkorange", "% Hispanic"),
-              "Percent Asian" = list(counties$asian, "darkviolet", "% Asian"))
-
-args$min = input$range[1]
-args$max = input$range[2]
-
-do.call(percent_map, args)
-  })
+  
+female_lexp = read.csv('../data/API_SP.DYN.LE00.FE.IN_DS2_en_csv_v2.csv', header = FALSE, skip = 4,stringsAsFactors = FALSE)  
+#   output$map = renderPlot( {
+# 
+# args = switch(input$var,
+#               "Percent White" = list(counties$white, "darkgreen", "% White"),
+#               "Percent Black" = list(counties$black, "black", "% Black"),
+#               "Percent Hispanic" = list(counties$hispanic, "darkorange", "% Hispanic"),
+#               "Percent Asian" = list(counties$asian, "darkviolet", "% Asian"))
+# 
+# args$min = input$range[1]
+# args$max = input$range[2]
+# 
+# do.call(percent_map, args)
+#   })
 
   
 })
--- project_01/shiny_app/ui.R
+++ project_01/shiny_app/ui.R
@@ -1,21 +1,48 @@
 library(shiny)
 
+currentTabPanel = "graph"
+
 shinyUI( 
   fluidPage(
     titlePanel("Life Expectancy Explorer"),
     sidebarLayout(
       sidebarPanel(
-        helpText("Create gender life expectancy graphs 
+        conditionalPanel(
+          condition = 'input.tab_views == "main_plot"',
+          helpText("Create gender life expectancy graphs 
                with information from The World Bank's DataBank."),
-        selectInput("var", 
-                  label = "Choose gender(s) to display",
-                  choices = c("Female", "Male"),
-                  selected = "Female",
-                  multiple = TRUE),
-      sliderInput("range",
-                  label = "Range of interest:",
-                  min=0, max=100,value=c(0,100))
+          checkboxGroupInput("gender_group",label = "Choose gender(s) to display",
+                             choices = list("Female" = 1, "Male" = 2),
+                             selected = 1
+            
+          ),
+          h5("Number of countries selected for box plot: "),
+          h5("Number of countries selected for linear plot: "),
+          radioButtons("radio", label = "Choose min/max values to display",
+                       choices = list("Min" = 1, "Max" = 2),
+                       selected = 1
+          )
+        ),
+        conditionalPanel(
+          condition = 'input.tab_views == "box_config"',
+          helpText("Select countries to display in the box plot.")
+        ),
+        conditionalPanel(
+          condition = 'input.tab_views == "linear_config"',
+          helpText("Select countries to display in the linear plot.")
+        )
+        
     ),
-    mainPanel(plotOutput("map"))
-    )
+    mainPanel(
+      tabsetPanel(
+        id = 'tab_views',
+        selected = NULL,
+        tabPanel('Plot', helpText("Plots go here."), value = 'main_plot'),
+        tabPanel('Countries for Boxplot', helpText("Countries configuration for boxplot goes here."), value = 'box_config'),
+        tabPanel('Countries for Linear', helpText("Countries configuration for linear goes here."), value = 'linear_config')         
+      )
+      
+#      plotOutput("map")
+    ) # mainPanel
+    ) # sidebarLayout
 ))
